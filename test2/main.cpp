9534a4228b7d1cf5a7d2c2b88759bcfc76152277
kullkay <ABulat2003@gmail.com>
2025-05-11 21:12:59 +0300

    Corrected pojects

--- test2/main.cpp
+++ test2/main.cpp
@@ -1,50 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include <unordered_map>
 #include <string>
-#include <vector>
-#include <regex>
 #include <thread>
 #include <mutex>
 #include <set>
-#include <mutex>
-
-
-std::pair<std::string, int> pars_flighting_string(const std::string& input) {
-    std::vector<std::regex> code_patterns = {
-        std::regex(R"(^[A-Z]{3})"),
-        std::regex(R"(^[A-Z]{2} ?)"),
-        std::regex(R"(^[0-9][A-Z] ?)"),
-        std::regex(R"(^[A-Z][0-9] ?)"),
-    };
-
-    std::regex number_pattern = std::regex(R"(^\d{1,5}$)");
-    
-    std::smatch match;
-    std::string code = "";
-    std::string number = "";
-
-    for (const auto& code_pattern : code_patterns) {
-        if (std::regex_search(input, match, code_pattern)) {
-            int code_size = match.str(0).size();
-            if (match.str(0)[code_size-1] == ' ')
-                code = match.str(0).substr(0, code_size-1);
-            else
-                code = match.str(0);
-
-            number = input.substr(code_size);
-            break;
-        }
-    }
-    if (number.empty())
-        number = input;
-
-    if (std::regex_search(number, match, number_pattern)) {
-        return {code, std::stoi(number)};
-    }
-
-    throw std::invalid_argument("error in line"+input);
-}
+#include "flighting_utils.h"
 
 
 struct FlightingRecords {
@@ -72,11 +32,11 @@ void process_file(
     std::ofstream outfile(output_filename);
 
     if (!infile.is_open()) {
-        std::cerr << "Не удалось открыть " << input_filename << std::endl;
+        std::cerr << "Failed open file: " << input_filename << std::endl;
         return;
     }
     if (!outfile.is_open()) {
-        std::cerr << "Не удалось открыть " << output_filename << std::endl;
+        std::cerr << "Failed open file: " << output_filename << std::endl;
         return;
     }
 
@@ -85,8 +45,6 @@ void process_file(
 
     if (db.add_records(pars_flighting_string(record)))
         outfile<<record<<'\n';
-    else 
-        std::cout<<"wrong"<<'\n';
 
     infile.close();
     outfile.close();
